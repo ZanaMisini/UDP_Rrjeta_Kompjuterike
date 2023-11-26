5276b97411c5b577f211338dfbf3533ab5bde21e
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-11-26 22:15:49 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+#include <winsock2.h>
+using namespace std;
 
+#pragma comment(lib,"ws2_32.lib") // Winsock Library
+#pragma warning(disable:4996) 
+
+#define BUFLEN 512
+#define PORT 8888
+
+int main()
+{
+    system("title UDP Server");
+
+    sockaddr_in server, client;
+
+   
+    WSADATA wsa;
+    printf("Initialising Winsock...");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Failed. Error Code: %d", WSAGetLastError());
+        exit(0);
+    }
+    printf("Initialised.\n");
+
+   
+    SOCKET server_socket;
+  
+    printf("Socket created.\n");
+
+   
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+ 
+    puts("Bind done.");
+
+    while (true)
+    {
+        printf("Waiting for data...");
+        fflush(stdout);
+        char message[BUFLEN] = {};
+
+        int message_len;
+        int slen = sizeof(sockaddr_in);
+        if (message_len = recvfrom(server_socket, message, BUFLEN, 0, (sockaddr*)&client, &slen) == SOCKET_ERROR)
+        {
+            printf("recvfrom() failed with error code: %d", WSAGetLastError());
+            exit(0);
+        }
+
+  
+        printf("Received packet from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        printf("Data: %s\n", message);
+
+        cin.getline(message, BUFLEN);
+
+     
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
+}
