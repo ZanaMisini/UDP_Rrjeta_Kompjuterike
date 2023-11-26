466f4e658ea85d5448e3d82b15b8a9ed5af959ac
ylljetakicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-11-26 23:04:52 +0100

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -2,6 +2,18 @@
 #include <winsock2.h>
 using namespace std;
 
+#pragma comment(lib,"ws2_32.lib") 
+#pragma warning(disable:4996) 
+
+#define SERVER "127.0.0.1"  // or "localhost" - ip address of UDP server
+#define BUFLEN 512  // max length of answer
+#define PORT 8888  // the port on which to listen for incoming data
+
+
+int main()
+{
+    system("title UDP Client");
+
 //VJOSA
 // start communication
     while (true)
