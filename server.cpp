4864b2f9285d408d152011f870c75c31f2500580
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-12-02 20:13:02 +0100

    append and write method fixed
    added extra comments, fixed the write and append method

--- server.cpp
+++ server.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <winsock2.h>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable : 4996)
+
+#define BUFLEN 512
+#define PORT 8888
+
 using namespace std;
 
+// Metoda per te shkruar ne file
 void writeFile(const string& filename, const string& content)
 {
     ofstream file(filename, ios::out | ios::binary);
@@ -16,6 +28,7 @@ void writeFile(const string& filename, const string& content)
     }
 }
 
+// Metoda per append ne file
 void appendFile(const string& filename, const string& content)
 {
     ofstream file(filename, ios::out | ios::app | ios::binary);
@@ -30,19 +43,12 @@ void appendFile(const string& filename, const string& content)
     }
 }
 
-#pragma comment(lib,"ws2_32.lib") // Winsock Library
-#pragma warning(disable:4996) 
-
-#define BUFLEN 512
-#define PORT 8888
-
 int main()
 {
+
     system("title UDP Server");
 
-    sockaddr_in server, client;
-
-   
+    // Inicializimi i winsock
     WSADATA wsa;
     printf("Initialising Winsock...");
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
@@ -52,69 +58,30 @@ int main()
     }
     printf("Initialised.\n");
 
-    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
-        {
-            printf("Could not create socket: %d", WSAGetLastError());
-        }
-    
-   
+    // Krijimi i socket
     SOCKET server_socket;
-  
+    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket: %d", WSAGetLastError());
+    }
     printf("Socket created.\n");
 
-    if(bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
-        {
-            printf("Bind faild with erorr code: %d", WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
-        
-   
+    // Pregatitim strukturen sockaddr_in
+    sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(PORT);
 
- 
+    // Lidhja
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
     puts("Bind done.");
-
-    if(sendto(server_socket, message, strlen(message), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-        {
-            printf("sendto() failed with error code: %d", WSAGetLastError());
-            return 3;
-        }
-
-     if (strncmp(message, "file r: ", 8) == 0)
-        {
-            
-            string filename = message + 8;
-
-           
-            ifstream file(filename, ios::binary);
-            if (!file.is_open())
-            {
-                cout << "Error opening file: " << filename << endl;
-
-                
-                string errorMsg = "Error opening file: " + filename;
-                if (sendto(server_socket, errorMsg.c_str(), errorMsg.size(), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-                {
-                    printf("sendto() failed with error code: %d", WSAGetLastError());
-                    return 3;
-                }
-                continue; 
-            }
-
-            
-            string fileContent((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-            file.close();
-
-            
-            if (sendto(server_socket, fileContent.c_str(), fileContent.size(), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-            {
-                printf("sendto() failed with error code: %d", WSAGetLastError());
-                return 3;
-            }
-        }
-   
+    // Ndihmojne per te mbajtur mend klientin e pare i cili i eshte casur serverit
+    unordered_set<string> firstClient;
+    unordered_map<string, sockaddr_in> clientAddresses;
 
     while (true)
     {
@@ -122,104 +89,16 @@ int main()
         fflush(stdout);
         char message[BUFLEN] = {};
 
+        // Per te marre te dhena nga klienti
+        sockaddr_in clientAddr;
+        int slen = sizeof(clientAddr);
         int message_len;
-        int slen = sizeof(sockaddr_in);
-        if (message_len = recvfrom(server_socket, message, BUFLEN, 0, (sockaddr*)&client, &slen) == SOCKET_ERROR)
+        if ((message_len = recvfrom(server_socket, message, BUFLEN, 0, (sockaddr*)&clientAddr, &slen)) == SOCKET_ERROR)
         {
             printf("recvfrom() failed with error code: %d", WSAGetLastError());
             exit(0);
         }
 
-        if (strncmp(message, "file w: ", 8) == 0)
-        {
-            if (allowedWriteClients.empty() || allowedWriteClients.count(clientKey) > 0)
-            {
-                string fileWriteCommand = message + 8;
-                size_t pos = fileWriteCommand.find(' ');
-                if (pos != string::npos)
-                {
-                    string filename = fileWriteCommand.substr(0, pos);
-                    string content = fileWriteCommand.substr(pos + 1);
+        printf("Received packet from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
 
-                    writeFile(filename, content);
-
-                    string successMsg = "Content written to file: " + filename;
-                    if (sendto(server_socket, successMsg.c_str(), successMsg.size(), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-                    {
-                        printf("sendto() failed with error code: %d", WSAGetLastError());
-                        return 3;
-                    }
-
-                    allowedWriteClients.insert(clientKey);
-                }
-            }
-
-            else
-            {
-                string errorMsg = "Write operation not allowed for this client";
-                if (sendto(server_socket, errorMsg.c_str(), errorMsg.size(), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-                {
-                    printf("sendto() failed with error code: %d", WSAGetLastError());
-                    return 3;
-                }
-            }
-        }
-            else if (strncmp(message, "file a: ", 8) == 0)
-        {
-            if (firstClient.empty() || firstClient.count(clientKey) > 0)
-            {
-                string fileAppendCommand = message + 8;
-                size_t pos = fileAppendCommand.find(' ');
-                if (pos != string::npos)
-                {
-                    string filename = fileAppendCommand.substr(0, pos);
-                    string content = fileAppendCommand.substr(pos + 1);
-
-                    appendFile(filename, content);
-
-                    string successMsg = "Content appended to file: " + filename;
-                    if (sendto(server_socket, successMsg.c_str(), successMsg.size(), 0, (sockaddr*)&clientAddr, sizeof(sockaddr_in)) == SOCKET_ERROR)
-                    {
-                        printf("sendto() failed with error code: %d", WSAGetLastError());
-                        return 3;
-                    }
-
-                    firstClient.insert(clientKey); 
-                }
-            }
-         else
-        {
-            printf("Client says: %s\n", message);
-
-            char serverMessage[BUFLEN];
-            printf("Enter message to client: ");
-            cin.getline(serverMessage, BUFLEN);
-
-            if (sendto(server_socket, serverMessage, strlen(serverMessage), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
-            {
-                printf("sendto() failed with error code: %d", WSAGetLastError());
-                return 3;
-            }
-        }
-    }
-
-    closesocket(server_socket);
-    WSACleanup();
-    return 0;
-}
-        
-
-  
-        printf("Received packet from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-        printf("Data: %s\n", message);
-
-        cin.getline(message, BUFLEN);
-
-     
-    }
-
-    
-
-    closesocket(server_socket);
-    WSACleanup();
-}
+        string clientKey = string(inet_ntoa(clientAddr.sin_addr)) + ":" + to_string(ntohs(clientAddr.sin_port));
