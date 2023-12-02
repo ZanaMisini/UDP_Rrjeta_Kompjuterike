f772cbc36fff05e1607344af8cf4b1ccb666fa05
Vlera Luma <118343087+VleraLuma@users.noreply.github.com>
2023-12-02 20:25:40 +0100

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,62 +1,8 @@
 #include <iostream>
 #include <winsock2.h>
-using namespace std;
 
-#pragma comment(lib,"ws2_32.lib") 
-#pragma warning(disable:4996) 
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable : 4996)
 
-#define SERVER "127.0.0.1"  // or "localhost" - ip address of UDP server
-#define BUFLEN 512  // max length of answer
-#define PORT 8888  // the port on which to listen for incoming data
-
-    int main()
-    {
-    system("title UDP Client");
-
-    WSADATA ws;
-    printf("Initialising Winsock...");
-    if (WSAStartup(MAKEWORD(2, 2), &ws) != 0)
-    {
-        printf("Failed. Error Code: %d", WSAGetLastError());
-        return 1;
-    }
-    printf("Initialised.\n");
-
-    sockaddr_in server;
-    int client_socket;
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) // <<< UDP socket
-    {
-        printf("socket() failed with error code: %d", WSAGetLastError());
-        return 2;
-    }
-
-    memset((char*)&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    server.sin_addr.S_un.S_addr = inet_addr(SERVER);
-
-    while (true)
-    {
-        char message[BUFLEN];
-        printf("Enter message: ");
-        cin.getline(message, BUFLEN);
-        if (sendto(client_socket, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR)
-        {
-            printf("sendto() failed with error code: %d", WSAGetLastError());
-            return 3;
-        }
-        char answer[BUFLEN] = {};
-        int slen = sizeof(sockaddr_in);
-        int answer_length;
-        if (answer_length = recvfrom(client_socket, answer, BUFLEN, 0, (sockaddr*)&server, &slen) == SOCKET_ERROR)
-        {
-            printf("recvfrom() failed with error code: %d", WSAGetLastError()) ;
-            exit(0);
-        }
-
-        cout << "Server says: " << answer << "\n" ;
-    }
-
-    closesocket(client_socket);
-    WSACleanup();
-}
+#define BUFLEN 512
+#define PORT 8888
